e0e7c749490b2ef0e73cac7482b4fc98fb49e356
Andrew Sou <andrewsou4@gmail.com>
2024-08-30 15:21:04 -0700

    Started career and about us page formatting

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,81 @@
 import React from "react";
 import "./About.css";
+import { motion } from "framer-motion";
+import { earthJPG } from "../Assets";
 
-export default function About() {
+export default function Home() {
   return (
-    <div className="pageContainer">
-      <h1>About Us</h1>
-      <p>Information about the organization will go here.</p>
+    <div
+      className="aboutUsContainer"
+      style={{ backgroundImage: `url(${earthJPG})` }}
+    >
+
+      <motion.div
+        className="titleSection"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <div className="aboutUsContent">
+          <motion.h1
+            className="aboutUsTitle"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 15.5, ease: "easeOut" }}
+          >
+            About ΑΚΨ
+          </motion.h1>
+          <motion.p
+            className="aboutus-subtitle1"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 16.5, ease: "easeOut", delay: 0.5 }}
+          >
+            
+          </motion.p>
+        </div>
+      </motion.div>
+
+      <div className="mainTitleSection">
+        <h1 className="mainTitle">What Is Alpha Kappa Psi?</h1>
+      </div>
+      <div className="aboutus-section">
+        <div>
+          <h2>Test -- An Image or Video will probably go here</h2>
+          <p>
+            The organization of Alpha Kappa Psi was founded in New York
+            University in 1904. Now over a century old, Alpha Kappa Psi has
+            helped over 300,000 individuals create lifelong friends and pursue
+            their dreams. Our chapter here at UC Irvine is devoted to this same
+            goal of helping students build lasting friendships and achieve their
+            dreams. Our goal is to help Anteaters become the best versions of
+            themselves that they can be.
+          </p>
+        </div>
+        <div>
+          <h2>The world’s oldest and largest business fraternity</h2>
+          <p>
+            The organization of Alpha Kappa Psi was founded in New York
+            University in 1904. Now over a century old, Alpha Kappa Psi has
+            helped over 300,000 individuals create lifelong friends and pursue
+            their dreams. Our chapter here at UC Irvine is devoted to this same
+            goal of helping students build lasting friendships and achieve their
+            dreams. Our goal is to help Anteaters become the best versions of
+            themselves that they can be.
+          </p>
+        </div>
+        <div>
+          <h2>Test -- An Image or Video will probably go here</h2>
+          <p>
+            The organization of Alpha Kappa Psi was founded in New York
+            University in 1904. Now over a century old, Alpha Kappa Psi has
+            helped over 300,000 individuals create lifelong friends and pursue
+            their dreams. why are u reading this omg u should be coding build
+            lasting friendships and achieve their dreams. Our goal is to help
+            Anteaters become the best versions of themselves that they can be.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Pages/Careers.jsx
+++ src/Pages/Careers.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import "./Careers.css";
+import { motion } from "framer-motion";
+import { earthJPG } from "../Assets";
 
-export default function Careers() {
+export default function Home() {
   return (
-    <div className="pageContainer">
-      <h1>Careers</h1>
-      <p>Details about career opportunities and support.</p>
+    <div
+      className="careersContainer"
+      style={{ backgroundImage: `url(${earthJPG})` }}
+    >
+
+      <motion.div
+        className="careerstitleSection"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <div className="careersContent">
+          <motion.h1
+            className="careersTitle"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 15.5, ease: "easeOut" }}
+          >
+            Careers
+          </motion.h1>
+          <motion.p
+            className="careers-subtitle1"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 16.5, ease: "easeOut", delay: 0.5 }}
+          >
+            
+          </motion.p>
+        </div>
+      </motion.div>
+
+      <div className="careersmainTitleSection">
+        <h1 className="careersmainTitle">Where have we worked?</h1>
+      </div>
+      <div className="careers-section">
+        <div>
+        
+        </div>
+      </div>
     </div>
   );
 }
