ea91e4229100c33b8c146e64c2cb3cd19f9ae38c
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2024-08-30 18:49:02 -0700

    updated video for no AKY logo and put instagram logo on top right, removed unnecessary files

--- src/Pages/Recruitment.jsx
+++ src/Pages/Recruitment.jsx
@@ -4,23 +4,25 @@ import { jacobPNG } from "../Assets";
 
 export default function Recruitment() {
   return (
-    <div 
+    <div
       className="recruitmentContainer"
       style={{
         backgroundImage: `url(${jacobPNG})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       <div className="heroSection">
         <h1 className="mainTitle">INSERT RUSH THEME</h1>
         <p className="subTitle">Fall Rush 2024</p>
-        <a href="#" className="rushButton">Rush Application</a>
+        <a href="#" className="rushButton">
+          Rush Application
+        </a>
       </div>
     </div>
   );
