464ecaa12a5dd3224684ce68e8ec15add119edce
Andrew Sou <andrewsou4@gmail.com>
2024-08-31 12:19:35 -0700

    More changes to table formatting

--- src/Pages/CareerDataRow.jsx
+++ src/Pages/CareerDataRow.jsx
@@ -1,15 +1,13 @@
+import "./CareerDataRow.css";
+
 export default function CareerDataRow({ data }) {
   console.log(data[0]);
   return (
-    <>
-      {
       <tr>
         <td>{data.Name}</td>
         <td>{data.Position}</td>
         <td>{data.Company}</td>
         <td>{data.Sector}</td>
       </tr>
-      }
-    </>
   );
 }
