c2a7a1ad69a924acdf6662b2c0fa0869b52d3fdd
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2024-08-30 23:56:41 -0700

    mobile optimization

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -2,36 +2,70 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavLinks.css";
 
-export default function NavLinks() {
+export default function NavLinks({ isMobile, closeMobileMenu }) {
+  const linkClass = isMobile ? "NavLink mobile" : "NavLink";
+  const handleClick = isMobile ? closeMobileMenu : () => {};
+
   return (
-    <ul className="nav-links">
+    <ul className={`nav-links ${isMobile ? "mobile" : ""}`}>
       <li>
-        <NavLink exact to="/" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          exact
+          to="/"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           Home
         </NavLink>
       </li>
       <li>
-        <NavLink to="/about" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          to="/about"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           About
         </NavLink>
       </li>
       <li>
-        <NavLink to="/meet-us" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          to="/meet-us"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           Meet Us
         </NavLink>
       </li>
       <li>
-        <NavLink to="/brotherhood" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          to="/brotherhood"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           Brotherhood
         </NavLink>
       </li>
       <li>
-        <NavLink to="/careers" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          to="/careers"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           Careers
         </NavLink>
       </li>
       <li>
-        <NavLink to="/recruitment" activeclassname="active" className={"NavLink"}>
+        <NavLink
+          to="/recruitment"
+          activeclassname="active"
+          className={linkClass}
+          onClick={handleClick}
+        >
           Recruitment
         </NavLink>
       </li>
