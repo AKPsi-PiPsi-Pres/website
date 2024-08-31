01514b45914dd9dca3cd0c6e086e371390a4b814
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2024-08-30 20:42:47 -0700

    colors made a lil nice and ALL NEWWW brotherhood pageeee

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -6,32 +6,32 @@ export default function NavLinks() {
   return (
     <ul className="nav-links">
       <li>
-        <NavLink exact to="/" activeclassname="active">
+        <NavLink exact to="/" activeclassname="active" className={"NavLink"}>
           Home
         </NavLink>
       </li>
       <li>
-        <NavLink to="/about" activeclassname="active">
+        <NavLink to="/about" activeclassname="active" className={"NavLink"}>
           About
         </NavLink>
       </li>
       <li>
-        <NavLink to="/meet-us" activeclassname="active">
+        <NavLink to="/meet-us" activeclassname="active" className={"NavLink"}>
           Meet Us
         </NavLink>
       </li>
       <li>
-        <NavLink to="/brotherhood" activeclassname="active">
+        <NavLink to="/brotherhood" activeclassname="active" className={"NavLink"}>
           Brotherhood
         </NavLink>
       </li>
       <li>
-        <NavLink to="/careers" activeclassname="active">
+        <NavLink to="/careers" activeclassname="active" className={"NavLink"}>
           Careers
         </NavLink>
       </li>
       <li>
-        <NavLink to="/recruitment" activeclassname="active">
+        <NavLink to="/recruitment" activeclassname="active" className={"NavLink"}>
           Recruitment
         </NavLink>
       </li>
