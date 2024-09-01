0a6c60350c661ebbfadffe5245e2cbc7e6e6aa6a
Aarush Inamdar <aarushinamdar@gmail.com>
2024-08-31 17:58:00 -0700

    added not found page and small tweaks to home and recruitment page

--- src/Components/SleekButton.jsx
+++ src/Components/SleekButton.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Button, styled } from "@mui/material";
 
 const SleekDarkButton = styled(Button)(({ theme }) => ({
-  background: "linear-gradient(45deg, #2196F3 40%, #21CBF7 90%)",
+  background: "linear-gradient(45deg, #1565C0 40%, #1976D2 90%)",
   border: 0,
-  borderRadius: 3,
+  borderRadius: 5,
   color: "#F5F5F5",
   height: 48,
   padding: "0 30px",
   transition: "all 0.3s ease-in-out",
   "&:hover": {
-    background: "linear-gradient(45deg, #21CBF3 30%, #2196F3 90%)",
-    boxShadow: "0 4px 6px 2px rgba(33, 203, 243, .5)",
+    background: "linear-gradient(45deg, #1976D2 30%, #1565C0 90%)",
+    boxShadow: "0 4px 6px 2px rgba(25, 118, 210, .5)",
     transform: "translateY(-2px)",
   },
 }));
