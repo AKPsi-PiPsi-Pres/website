886b1d69883836555f3311115834ed6c4f7f54b1
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2024-09-07 16:20:38 -0700

    visual upgrades to small sizing and decoration stuff

--- src/Pages/ExecutiveBoardList.jsx
+++ src/Pages/ExecutiveBoardList.jsx
@@ -13,7 +13,10 @@ export default function ExecutiveBoardList({ brothers }) {
             (brother) =>
               brother.length > 0 && (
                 <div key={brother[0]} className={styles.brotherCard}>
-                  <Link to={`/${brother[0].replace(" ", "-")}`}>
+                  <Link
+                    to={`/${brother[0].replace(" ", "-")}`}
+                    className={styles.brotherLink}
+                  >
                     <div className={styles.imageWrapper}>
                       <img
                         src={
