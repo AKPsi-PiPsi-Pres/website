61f3b0ba069b2ae24b8aa823eb85ffe3d731a5a5
Aarush Inamdar <aarushinamdar@gmail.com>
2024-09-08 15:41:15 -0700

    minor career page updates

--- src/Pages/Careers.jsx
+++ src/Pages/Careers.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import { alumni1 } from "../Assets";
 import CareerTable from "./CareerTable";
 import CareerLogoScroller from "./CareerLogoScroll";
-import DownPointerButton from "../Components/DownPointerButton";
 
 export default function Careers() {
   return (
@@ -18,21 +17,8 @@ export default function Careers() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <div className="careersContent">
-          <motion.h1
-            className="hero-title"
-            initial={{ y: -150, opacity: 0 }}
-            animate={{ y: -150, opacity: 1 }}
-            transition={{ duration: 1.5, ease: "easeOut" }}
-          >
+        <div className="hero-title" style={{"top": "-150px"}}>
             Careers
-          </motion.h1>
-          <motion.p
-            className="careers-subtitle1"
-            initial={{ y: 50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 2.5, ease: "easeOut", delay: 0.5 }}
-          ></motion.p>
         </div>
       </motion.div>
 
