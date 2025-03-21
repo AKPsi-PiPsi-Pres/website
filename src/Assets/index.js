a624eddd6adbd6abd0122a3d5d60a38b296cc5c4
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2025-03-21 14:44:13 -0700

    HAUBERRRRRR

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -153,6 +153,7 @@ export { default as TripleALogo } from "./Logos/TripleALogo.png";
 export { default as RadixWellnessLogo } from "./Logos/RadixWellnessLogo.jpeg";
 export { default as RumeLogo } from "./Logos/RumeLogo.png";
 export { default as VerizonLogo } from "./Logos/VerizonLogo.png";
+export { default as CapitalGroupLogo } from "./Logos/CapitalGroupLogo.png";
 
 export { default as Fall23RushVideo } from "./Videos/FallRush2023Website.mp4";
 export { default as CruiseReel } from "./Videos/CruiseReelWebsite.mp4";
